f593ad9cb9558fb93f27447068586b28de9cdd5b
kbowser <56522658+kristinbowser@users.noreply.github.com>
2020-05-05 13:47:05 -0400

    styled dropdown btn, added sticky nav, added contact form in footer

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,3 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickNav()};
-
-// Get the navbar
-var navbar = document.getElementById("navigation");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickNav() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -33,3 +14,21 @@ function myFunction() {
       }
     }
   }
+
+  // When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickNav()};
+
+// Get the navbar
+var navbar = document.getElementById("navBar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
