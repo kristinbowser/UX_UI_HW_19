0bbd58fe4e4bed26dd41373af58486f0d2dddd40
kbowser <56522658+kristinbowser@users.noreply.github.com>
2020-05-05 21:13:49 -0400

    sticky fixed

--- JS/index.js
+++ JS/index.js
@@ -27,7 +27,10 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    // $(".logo img").css("height", "10%");
+    $(".logo img").css("max-width", "50px");
+    $("#navigation").css("margin-top", "1%");
   } else {
     navbar.classList.remove("sticky");
   }
